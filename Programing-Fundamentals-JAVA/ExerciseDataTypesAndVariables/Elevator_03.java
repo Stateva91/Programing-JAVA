675f548a5c068d8f3215ae6b5c96d70b4e60d6af
Snezhana Stateva <114188863+Stateva91@users.noreply.github.com>
2023-01-16 13:04:21 +0100

    update tasks

--- Programing-Fundamentals-JAVA/ExerciseDataTypesAndVariables/Elevator_03.java
+++ Programing-Fundamentals-JAVA/ExerciseDataTypesAndVariables/Elevator_03.java
@@ -3,8 +3,16 @@ package ExerciseDataTypesAndVariables;
 import java.util.Scanner;
 
 public class Elevator_03 {
-
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
+
+        int people=Integer.parseInt(scanner.nextLine());
+        int capacity=Integer.parseInt(scanner.nextLine());
+
+        double course=1.0*people/capacity;
+        int  fullCourses= (int) Math.ceil(course);
+
+        System.out.println(fullCourses);
+
     }
 }
--- Programing-Fundamentals-JAVA/ExerciseDataTypesAndVariables/WaterOverflow_07.java
+++ Programing-Fundamentals-JAVA/ExerciseDataTypesAndVariables/WaterOverflow_07.java
@@ -1,4 +1,24 @@
 package ExerciseDataTypesAndVariables;
 
+import java.util.Scanner;
+
 public class WaterOverflow_07 {
-}
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int  waterTank=255;
+        int n=Integer.parseInt(scanner.nextLine());
+        int currentLiters = 0;
+
+        for (int i = 0; i <n ; i++) {
+            int pouredLiters = Integer.parseInt(scanner.nextLine());
+            currentLiters+=pouredLiters;
+            if(currentLiters>waterTank){
+                System.out.println("Insufficient capacity!");
+                currentLiters -= pouredLiters;
+            }
+        }
+        System.out.println(currentLiters);
+    }
+            
+        }
+

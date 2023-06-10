fa8fb0f5d225dd726a259cebd12c60e4ad4cbc96
Snezhana Stateva <114188863+Stateva91@users.noreply.github.com>
2023-06-10 12:57:29 +0200

    new tasks

---
+++ Programing-Advanced-JAVA/IteratorsAndComparatorsExercises/IteratorsComparatorsStrategyPattern_06/PersonAgeComparator.java
@@ -0,0 +1,10 @@
+package IteratorsComparatorsStrategyPattern_06;
+
+import java.util.Comparator;
+
+public class PersonAgeComparator implements Comparator<Person> {
+    @Override
+    public int compare(Person first, Person second) {
+        return first.getAge() - second.getAge();
+    }
+}

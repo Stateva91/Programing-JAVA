bf5f98f9b2f911c0b90621f5ec42610122980b41
Snezhana Stateva <114188863+Stateva91@users.noreply.github.com>
2023-11-09 20:06:31 +0100

    Hibernate Code First

---
+++ Spring Data/EntityRelations/src/main/java/entities/Car.java
@@ -0,0 +1,24 @@
+package entities;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="cars")
+public class Car extends PassengerVehicle{
+    private static final String CAR_TYPE= "CAR";
+
+    public Car(){
+        super(CAR_TYPE);
+    }
+
+    public Car(String model, String fuelType, int seats){
+            this(); //izvikvame konstruktora
+
+            this.model=model;
+            this.fuelType=fuelType;
+            this.seats=seats;
+    }
+
+}

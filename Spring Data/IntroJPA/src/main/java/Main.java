42a3e685ed4c7c7ae674356918c97f9ff5666531
Snezhana Stateva <114188863+Stateva91@users.noreply.github.com>
2024-02-23 07:57:25 +0100

    Introduction to Hibernate

---
+++ Spring Data/IntroJPA/src/main/java/Main.java
@@ -0,0 +1,23 @@
+
+import entities.Teacher;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory emf =
+                Persistence.createEntityManagerFactory("school");
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+//        Teacher teacher = new Teacher();
+//        teacher.setName("Teo");
+//        em.persist(teacher);
+
+        Teacher fromDB=em.find(Teacher.class, 3);
+        System.out.println(fromDB);
+        em.getTransaction().commit();
+    }
+
+}

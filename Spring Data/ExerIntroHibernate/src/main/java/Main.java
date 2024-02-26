635b636f0b7fbe661f7a62d754adc074a9911f40
Snezhana Stateva <114188863+Stateva91@users.noreply.github.com>
2024-02-26 14:48:02 +0100

    Exercise: Introduction to Hibernate

--- Spring Data/ExerIntroHibernate/src/main/java/Main.java
+++ Spring Data/ExerIntroHibernate/src/main/java/Main.java
@@ -6,9 +6,11 @@ import entities.Town;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
 import java.util.Comparator;
 import java.util.List;
 
@@ -28,8 +30,21 @@ public class Main {
       //  AddingANewAddressAndUpdatingEmployee_6(entityManager);
         // AddressesWithEmployeeCount_7(entityManager);
         //GetEmployeeWithProject_8(entityManager);
-        FindLatest10Projects_9(entityManager);
+        //FindLatest10Projects_9(entityManager);
+        IncreaseSalaries_10(entityManager);
         entityManager.getTransaction().commit();
+    }
+
+    private static void IncreaseSalaries_10(EntityManager entityManager) {
+        List<Employee> employees = entityManager.createQuery("SELECT e FROM Employee e JOIN e.department d " +
+                "WHERE  d.name IN ('Engineering', 'Tool Design', 'Marketing', 'Information Services')", Employee.class)
+                .getResultList();
+
+        for (Employee employee:employees){
+            employee.setSalary(employee.getSalary().multiply(BigDecimal.valueOf(1.12)));
+            System.out.printf("%s %s ($%.2f)%n", employee.getFirstName(), employee.getLastName(), employee.getSalary());
+        }
+
     }
 
     private static void FindLatest10Projects_9(EntityManager entityManager) {

1e852d357c72ad7643b55d90fbaea35a34371089
Snezhana Stateva <114188863+Stateva91@users.noreply.github.com>
2024-03-01 14:39:44 +0100

    Spring Data Intro

---
+++ Spring Data/SpringDataIntro/src/main/java/com/example/springdataintro/entities/BaseEntity.java
@@ -0,0 +1,22 @@
+package com.example.springdataintro.entities;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    protected BaseEntity(){
+
+    }
+
+    public int getId(){
+        return id;
+    }
+}

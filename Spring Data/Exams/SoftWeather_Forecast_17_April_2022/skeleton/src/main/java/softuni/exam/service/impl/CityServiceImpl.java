1ae0506005deb499e3a3f69559603d629d10e4bd
Snezhana Stateva <114188863+Stateva91@users.noreply.github.com>
2024-03-08 12:28:40 +0100

    Exam - 17 April 2022

--- Spring Data/Exams/SoftWeather_Forecast_17_April_2022/skeleton/src/main/java/softuni/exam/service/impl/CityServiceImpl.java
+++ Spring Data/Exams/SoftWeather_Forecast_17_April_2022/skeleton/src/main/java/softuni/exam/service/impl/CityServiceImpl.java
@@ -1,30 +1,42 @@
 package softuni.exam.service.impl;
 
+import com.google.gson.Gson;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import softuni.exam.repository.CityRepository;
 import softuni.exam.service.CityService;
+import softuni.exam.util.ValidationUtils;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Service
 public class CityServiceImpl implements CityService {
+    public static final Path CITIES_PATH=Path.of("C:\\Users\\sneji\\Downloads\\SoftWeather Forecast_Skeleton\\skeleton\\src\\main\\resources\\files\\json\\cities.json");
 
     private final CityRepository cityRepository;
+    private final Gson gson;
+    private final ValidationUtils validationUtils;
+    private final ModelMapper modelMapper;
 
     @Autowired
-    public CityServiceImpl(CityRepository cityRepository) {
+    public CityServiceImpl(CityRepository cityRepository, Gson gson, ValidationUtils validationUtils, ModelMapper modelMapper) {
         this.cityRepository = cityRepository;
+        this.gson = gson;
+        this.validationUtils = validationUtils;
+        this.modelMapper = modelMapper;
     }
 
     @Override
     public boolean areImported() {
-        return false;
+        return this.cityRepository.count()>0;
     }
 
     @Override
     public String readCitiesFileContent() throws IOException {
-        return null;
+        return Files.readString(CITIES_PATH);
     }
 
     @Override

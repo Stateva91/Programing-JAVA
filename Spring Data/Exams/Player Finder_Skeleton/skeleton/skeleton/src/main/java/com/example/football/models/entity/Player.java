3031616509c2a5e5493f7e34195ebbbd5f117542
Snezhana Stateva <114188863+Stateva91@users.noreply.github.com>
2024-03-12 13:53:04 +0100

    Exam - 1 August 2021

--- Spring Data/Exams/Player Finder_Skeleton/skeleton/skeleton/src/main/java/com/example/football/models/entity/Player.java
+++ Spring Data/Exams/Player Finder_Skeleton/skeleton/skeleton/src/main/java/com/example/football/models/entity/Player.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -40,4 +41,17 @@ public class Player {
 
     @OneToOne
     private Stat stat;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return Objects.equals(id, player.id) && Objects.equals(email, player.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email);
+    }
 }
--- Spring Data/Exams/Player Finder_Skeleton/skeleton/skeleton/src/main/java/com/example/football/service/impl/TownServiceImpl.java
+++ Spring Data/Exams/Player Finder_Skeleton/skeleton/skeleton/src/main/java/com/example/football/service/impl/TownServiceImpl.java
@@ -1,23 +1,38 @@
 package com.example.football.service.impl;
 
+import com.example.football.repository.TownRepository;
 import com.example.football.service.TownService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 
 //ToDo - Implement all methods
 @Service
 public class TownServiceImpl implements TownService {
 
+    @Autowired
+    private final TownRepository townRepository;
+
+    public TownServiceImpl(TownRepository townRepository) {
+        this.townRepository = townRepository;
+    }
+
 
     @Override
     public boolean areImported() {
-        return false;
+        return this.townRepository.count()>0;
     }
 
     @Override
-    public String readTownsFileContent() {
-        return null;
+    public String readTownsFileContent() throws IOException {
+        Path path=Path.of("C:\\Users\\sneji\\Downloads\\Player Finder_Skeleton\\skeleton\\skeleton\\src\\main\\resources\\files\\json\\towns.json");
+        return String.join("\n", Files.readAllLines(path));
     }
+
 
     @Override
     public String importTowns() {
